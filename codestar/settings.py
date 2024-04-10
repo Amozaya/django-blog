2dc1b74cd42839cfc4ef280ab9483c3d26876098
Oleg Fursa <olegfursa93@hotmail.com>
2024-04-10 21:31:53 +0000

    get app ready for deploy

--- codestar/settings.py
+++ codestar/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure---lu9y2j)-ah^gb4w85$*x6r+b*s(=@p3d6oz*kb8mz6!cywjz'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['8000-amozaya-djangoblog-xxmde977xpd.ws-eu110.gitpod.io']
+ALLOWED_HOSTS = ['8000-amozaya-djangoblog-xxmde977xpd.ws-eu110.gitpod.io','.herokuapp.com']
 
 
 # Application definition
